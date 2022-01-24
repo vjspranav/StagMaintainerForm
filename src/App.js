b845773145ebcf5b58a5c027fd546348794e39be
b30wulffz <shlokpandey123@gmail.com>
2022-01-24 18:11:31 +0530

    Ui tweaks

--- src/App.js
+++ src/App.js
@@ -65,7 +65,7 @@ function App() {
     vendor: Yup.string().url("Invalid URL").required("Required"),
     common_tree: Yup.string().url("Invalid URL"),
     common_vendor: Yup.string().url("Invalid URL"),
-    // Checl comma separated list of other dependencies if each is url
+    // Check comma separated list of other dependencies if each is url
     other_dependencies: Yup.string(),
     // selinux_status enforcing or permissive
     selinux_status: Yup.string().required("Required"),
@@ -79,8 +79,8 @@ function App() {
       // justifyContent: "center",
       margin: 0,
       padding: 0,
-      width: "100vw",
-      height: "120vh",
+      overflow: "auto",
+      background: "#0ed2f7",
     },
   };
 
@@ -90,87 +90,112 @@ function App() {
     alert(JSON.stringify(fields, null, 2));
   };
 
+  const [width, setWidth] = React.useState(window.innerWidth);
+
+  const handleWindowSizeChange = () => {
+    setWidth(window.innerWidth);
+  };
+
+  React.useEffect(() => {
+    window.addEventListener("resize", handleWindowSizeChange);
+    return () => {
+      window.removeEventListener("resize", handleWindowSizeChange);
+    };
+  }, []);
+
+  const isMobile = width <= 768;
+
   return (
     <div style={styles.root}>
       {/* Add a heading and a link in menu */}
-      <AppBar position="static" color="primary">
+      {/* <AppBar position="static" color="primary">
         <h2 style={{ marginLeft: "10px" }}>Stag Maintainer Application</h2>
-      </AppBar>
-      <Formik
-        style={{ marginTop: "10px", backgroundColor: "gray" }}
-        initialValues={initialValues}
-        validationSchema={validationSchema}
-        onSubmit={onSubmit}
+      </AppBar> */}
+      <div
+        style={{
+          textAlign: "center",
+          margin: "40px 0",
+          fontSize: "2rem",
+          color: "white",
+          fontWeight: "600",
+          // textShadow:
+          // "-0.4px -0.4px 0 #000, 0.4px -0.4px 0 #000, -0.4px 0.4px 0 #000, 0.4px 0.4px 0 #000",
+        }}
       >
-        {({
-          values,
-          errors,
-          touched,
-          isSubmitting,
-          handleChange,
-          handleBlur,
-        }) => (
-          <Form
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              marginTop: "10px",
-              marginLeft: "10px",
-            }}
-          >
-            <div>
+        Stag Maintainer Application
+      </div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          // margin: isMobile ? "0" : "40px 0",
+          marginBottom: isMobile ? "10px" : "40px",
+        }}
+      >
+        <Formik
+          initialValues={initialValues}
+          validationSchema={validationSchema}
+          onSubmit={onSubmit}
+        >
+          {({
+            values,
+            errors,
+            touched,
+            isSubmitting,
+            handleChange,
+            handleBlur,
+          }) => (
+            <Form
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                minWidth: isMobile ? "100vw" : "800px",
+                padding: "20px",
+                boxShadow:
+                  "rgba(255, 255, 255, 0.4) -5px 5px, rgba(255, 255, 255, 0.3) -10px 10px, rgba(255, 255, 255, 0.2) -15px 15px, rgba(255, 255, 255, 0.1) -20px 20px, rgba(255, 255, 255, 0.05) -25px 25px",
+                boxSizing: "border-box",
+                background: "white",
+              }}
+            >
               <TextField
-                style={{ width: "50%" }}
+                // style={{ width: "50%" }}
                 id="name"
                 label="Name"
                 name="name"
                 value={values.name}
                 onChange={handleChange}
                 onBlur={handleBlur}
-                helperText={
-                  touched.name ? errors.name : "Please enter your name"
-                }
+                helperText={touched.name ? errors.name : ""}
                 error={touched.name && Boolean(errors.name)}
                 margin="normal"
               />
-            </div>
-            <div>
               <TextField
-                style={{ width: "50%" }}
+                // style={{ width: "50%" }}
                 id="tg_username"
                 label="Telegram Username"
                 name="tg_username"
                 value={values.tg_username}
                 onChange={handleChange}
                 onBlur={handleBlur}
-                helperText={
-                  touched.tg_username
-                    ? errors.tg_username
-                    : "Please enter your Telegram username"
-                }
+                helperText={touched.tg_username ? errors.tg_username : ""}
                 error={touched.tg_username && Boolean(errors.tg_username)}
                 margin="normal"
               />
-            </div>
-            <div>
               <TextField
-                style={{ width: "50%" }}
+                // style={{ width: "50%" }}
                 id="email"
                 label="Email"
                 name="email"
                 value={values.email}
                 onChange={handleChange}
                 onBlur={handleBlur}
-                helperText={
-                  touched.email ? errors.email : "Please enter your email"
-                }
+                helperText={touched.email ? errors.email : ""}
                 error={touched.email && Boolean(errors.email)}
                 margin="normal"
               />
-            </div>
-            <div>
               <TextField
-                style={{ width: "50%" }}
+                // style={{ width: "50%" }}
                 id="github_username"
                 label="Github Username"
                 name="github_username"
@@ -178,9 +203,7 @@ function App() {
                 onBlur={handleBlur}
                 value={values.github_username}
                 helperText={
-                  touched.github_username
-                    ? errors.github_username
-                    : "Please enter your github username"
+                  touched.github_username ? errors.github_username : ""
                 }
                 error={
                   touched.github_username && Boolean(errors.github_username)
@@ -188,34 +211,26 @@ function App() {
                 variant="outlined"
                 margin="normal"
               />
-            </div>
-            <div>
               <TextField
-                style={{ width: "50%" }}
+                // style={{ width: "50%" }}
                 id="device_name"
                 label="Device Name"
                 name="device_name"
                 onChange={handleChange}
                 onBlur={handleBlur}
                 value={values.device_name}
-                helperText={
-                  touched.device_name
-                    ? errors.device_name
-                    : "Please enter your device name (For Ex: OnePlus 9 Pro)"
-                }
+                helperText={touched.device_name ? errors.device_name : ""}
                 error={touched.device_name && Boolean(errors.device_name)}
                 variant="outlined"
                 margin="normal"
               />
-            </div>
-            <div>
               <Autocomplete
                 id="device_company_autoaomplete"
                 options={options}
                 renderInput={(params) => (
                   <TextField
                     {...params}
-                    style={{ width: "50%" }}
+                    // style={{ width: "50%" }}
                     id="device_company"
                     label="Device Company"
                     name="device_company"
@@ -223,9 +238,7 @@ function App() {
                     onBlur={handleBlur}
                     value={values.device_company}
                     helperText={
-                      touched.device_company
-                        ? errors.device_company
-                        : "Please enter your device company (For Ex: Google)"
+                      touched.device_company ? errors.device_company : ""
                     }
                     error={
                       touched.device_company && Boolean(errors.device_company)
@@ -235,10 +248,8 @@ function App() {
                   />
                 )}
               />
-            </div>
-            <div>
               <TextField
-                style={{ width: "50%" }}
+                // style={{ width: "50%" }}
                 id="device_codename"
                 label="Device Codename"
                 name="device_codename"
@@ -246,9 +257,7 @@ function App() {
                 onBlur={handleBlur}
                 value={values.device_codename}
                 helperText={
-                  touched.device_codename
-                    ? errors.device_codename
-                    : "Please enter your device codename (For Ex: lemonadep)"
+                  touched.device_codename ? errors.device_codename : ""
                 }
                 error={
                   touched.device_codename && Boolean(errors.device_codename)
@@ -256,106 +265,74 @@ function App() {
                 variant="outlined"
                 margin="normal"
               />
-            </div>
-            <div>
               <TextField
-                style={{ width: "50%" }}
+                // style={{ width: "50%" }}
                 id="device_tree"
                 label="Device Tree"
                 name="device_tree"
                 onChange={handleChange}
                 onBlur={handleBlur}
                 value={values.device_tree}
-                helperText={
-                  touched.device_tree
-                    ? errors.device_tree
-                    : "Please enter your device tree url"
-                }
+                helperText={touched.device_tree ? errors.device_tree : ""}
                 error={touched.device_tree && Boolean(errors.device_tree)}
                 variant="outlined"
                 margin="normal"
               />
-            </div>
-            <div>
               <TextField
-                style={{ width: "50%" }}
+                // style={{ width: "50%" }}
                 id="kernel"
                 label="Kernel"
                 name="kernel"
                 onChange={handleChange}
                 onBlur={handleBlur}
                 value={values.kernel}
-                helperText={
-                  touched.kernel
-                    ? errors.kernel
-                    : "Please enter your kernel tree link"
-                }
+                helperText={touched.kernel ? errors.kernel : ""}
                 error={touched.kernel && Boolean(errors.kernel)}
                 variant="outlined"
                 margin="normal"
               />
-            </div>
-            <div>
               <TextField
-                style={{ width: "50%" }}
+                // style={{ width: "50%" }}
                 id="vendor"
                 label="Vendor"
                 name="vendor"
                 onChange={handleChange}
                 onBlur={handleBlur}
                 value={values.vendor}
-                helperText={
-                  touched.vendor
-                    ? errors.vendor
-                    : "Please enter your vendor github link"
-                }
+                helperText={touched.vendor ? errors.vendor : ""}
                 error={touched.vendor && Boolean(errors.vendor)}
                 variant="outlined"
                 margin="normal"
               />
-            </div>
-            <div>
               <TextField
-                style={{ width: "50%" }}
+                // style={{ width: "50%" }}
                 id="common_tree"
                 label="Common Tree (Optional)"
                 name="common_tree"
                 onChange={handleChange}
                 onBlur={handleBlur}
                 value={values.common_tree}
-                helperText={
-                  touched.common_tree
-                    ? errors.common_tree
-                    : "Please enter your common tree link"
-                }
+                helperText={touched.common_tree ? errors.common_tree : ""}
                 error={touched.common_tree && Boolean(errors.common_tree)}
                 variant="outlined"
                 margin="normal"
               />
-            </div>
-            <div>
               <TextField
-                style={{ width: "50%" }}
+                // style={{ width: "50%" }}
                 id="common_vendor"
                 label="Common Vendor (Optional)"
                 name="common_vendor"
                 onChange={handleChange}
                 onBlur={handleBlur}
                 value={values.common_vendor}
-                helperText={
-                  touched.common_vendor
-                    ? errors.common_vendor
-                    : "Please enter your common vendor link"
-                }
+                helperText={touched.common_vendor ? errors.common_vendor : ""}
                 error={touched.common_vendor && Boolean(errors.common_vendor)}
                 variant="outlined"
                 margin="normal"
               />
-            </div>
-            <div>
               {/* Take , separated links and store as array */}
               <TextField
-                style={{ width: "50%" }}
+                // style={{ width: "50%" }}
                 id="other_dependencies"
                 label="Other Dependencies (Optional)"
                 name="other_dependencies"
@@ -363,9 +340,7 @@ function App() {
                 onBlur={handleBlur}
                 value={values.other_dependencies}
                 helperText={
-                  touched.other_dependencies
-                    ? errors.other_dependencies
-                    : "Please enter your other dependencies"
+                  touched.other_dependencies ? errors.other_dependencies : ""
                 }
                 error={
                   touched.other_dependencies &&
@@ -374,12 +349,10 @@ function App() {
                 variant="outlined"
                 margin="normal"
               />
-            </div>
-            <div>
               {/* Select field with Enforcing and Permissive for selinux_status */}
               <FormControl
                 variant="outlined"
-                style={{ width: "50%" }}
+                // style={{ width: "50%" }}
                 margin="normal"
               >
                 <InputLabel id="selinux_status">SELinux Status</InputLabel>
@@ -395,8 +368,6 @@ function App() {
                   <MenuItem value="permissive">Permissive</MenuItem>
                 </Select>
               </FormControl>
-            </div>
-            <div>
               {/* Submit Button */}
               <Button
                 type="submit"
@@ -406,10 +377,10 @@ function App() {
               >
                 Submit
               </Button>
-            </div>
-          </Form>
-        )}
-      </Formik>
+            </Form>
+          )}
+        </Formik>
+      </div>
     </div>
   );
 }
