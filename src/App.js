d45a8983e9b27da99a8a3bd6cfd80deb6eb5b86e
vjspranav <pranavasri@live.in>
2022-01-25 14:19:47 +0530

    Add other dependecies properly

--- src/App.js
+++ src/App.js
@@ -147,6 +147,7 @@ function Main() {
   const onSubmit = (fields, { setSubmitting }) => {
     setSubmitting(false);
     console.log(fields);
+    fields.other_dependencies = fields.other_dependencies.split(",");
     axios
       .post("https://api.stag-os.org/maintainers/apply", fields)
       .then((response) => {
